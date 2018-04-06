80a48a4f594fad0a1464008a71de63700b9a88e6
Mel Vasu <malavika.vasudevan@myob.com>
2018-04-06 10:31:50 +1000

    Added more nextgen test given allAliveCells and givenNonSquareWorld

--- src/test/NextGenerationClassTest.java
+++ src/test/NextGenerationClassTest.java
@@ -29,4 +29,30 @@ public class NextGenerationClassTest {
         Assert.assertFalse(nextWorld.getWorldGrid()[1][1].getState());
     }
 
+    @Test
+    public void givenAllAliveCellsReturnAllLiveCells() {
+        boolean[][] arr = {{true,true},{true, true}};
+        World world = new World(arr);
+        World nextWorld = NextGeneration.getNextWorld(world);
+        Assert.assertTrue(nextWorld.getWorldGrid()[0][0].getState());
+        Assert.assertTrue(nextWorld.getWorldGrid()[0][1].getState());
+        Assert.assertTrue(nextWorld.getWorldGrid()[1][0].getState());
+        Assert.assertTrue(nextWorld.getWorldGrid()[1][1].getState());
+    }
+
+    @Test
+    public void givenNonSquareWorldComputeNextGenerationSuccessfully() {
+        boolean[][] arr = {{true,false,true,false},{false,true, true,true}};
+        World world = new World(arr);
+        World nextWorld = NextGeneration.getNextWorld(world);
+        Assert.assertTrue(nextWorld.getWorldGrid()[0][0].getState());
+        Assert.assertFalse(nextWorld.getWorldGrid()[0][1].getState());
+        Assert.assertTrue(nextWorld.getWorldGrid()[0][2].getState());
+        Assert.assertFalse(nextWorld.getWorldGrid()[0][3].getState());
+        Assert.assertTrue(nextWorld.getWorldGrid()[1][0].getState());
+        Assert.assertTrue(nextWorld.getWorldGrid()[1][1].getState());
+        Assert.assertTrue(nextWorld.getWorldGrid()[1][2].getState());
+        Assert.assertTrue(nextWorld.getWorldGrid()[1][3].getState());
+    }
+
 }

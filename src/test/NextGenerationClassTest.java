39fa32587283f59856c9475d3abafd06de782beb
Mel Vasu <malavika.vasudevan@myob.com>
2018-04-06 10:08:16 +1000

    Added next generation test - given square and dead world

--- src/test/NextGenerationClassTest.java
+++ src/test/NextGenerationClassTest.java
@@ -1,4 +1,32 @@
 package test;
 
+import main.NextGeneration;
+import main.World;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class NextGenerationClassTest {
+
+    @Test
+    public void givenSquareWorldGComputesNextGenerationSuccessfully() {
+        boolean[][] arr = {{false,true},{true, false}};
+        World world = new World(arr);
+        World nextWorld = NextGeneration.getNextWorld(world);
+        Assert.assertFalse(nextWorld.getWorldGrid()[0][0].getState());
+        Assert.assertFalse(nextWorld.getWorldGrid()[0][1].getState());
+        Assert.assertFalse(nextWorld.getWorldGrid()[1][0].getState());
+        Assert.assertFalse(nextWorld.getWorldGrid()[1][1].getState());
+    }
+
+    @Test
+    public void givenAllDeadWorldGivesDeadWorld() {
+        boolean[][] arr = {{false,false},{false, false}};
+        World world = new World(arr);
+        World nextWorld = NextGeneration.getNextWorld(world);
+        Assert.assertFalse(nextWorld.getWorldGrid()[0][0].getState());
+        Assert.assertFalse(nextWorld.getWorldGrid()[0][1].getState());
+        Assert.assertFalse(nextWorld.getWorldGrid()[1][0].getState());
+        Assert.assertFalse(nextWorld.getWorldGrid()[1][1].getState());
+    }
+
 }
